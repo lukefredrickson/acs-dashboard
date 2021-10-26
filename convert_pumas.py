52518edf77fe6cab1e894056d04a2466da168e0c
Luke Fredrickson <lukeefredrickson@gmail.com>
2021-10-25 20:09:41 -0400

    refactored download scripts and added many more comments

--- convert_pumas.py
+++ convert_pumas.py
@@ -1,15 +1,14 @@
 import requests
 import os
 import sys
+from utils.globals import *
 
-with open("./puma_file_names.txt") as file_names:
+# script globals
+current_file = ""  # current file being worked on
+json_file_path = ""  # write-path of the current file
+# read in filenames to convert from text file
+with open(PUMA_FILE_NAMES) as file_names:
     files = file_names.read().splitlines()
-
-converter = "http://ogre.adc4gis.com/convert"
-data_path = "./data/pumas/"
-zip_path = "./data/pumas/zip/"
-current_file = ""
-json_file_path = ""
 
 
 def main():
@@ -18,8 +17,10 @@ def main():
         global json_file_path
 
         current_file = file_name
+        # get the base filename from zip file, then join with .json extension for output name
         json_file_name = (os.path.splitext(file_name)[0] + '.json')
-        json_file_path = os.path.join(data_path, json_file_name)
+        # join json output name with output directory for full write-path
+        json_file_path = os.path.join(PUMAS_GEOJSON_DIRECTORY, json_file_name)
 
         print("Converting '%s' to geojson" % file_name, end="")
 
@@ -27,15 +28,17 @@ def main():
             print(" ... geojson already exists")
             continue
 
-        if os.path.exists(os.path.join(zip_path, file_name)):
-            shapefile = open(os.path.join(zip_path, file_name), 'rb')
+        if os.path.exists(os.path.join(PUMAS_ZIP_FILE_DIRECTORY, file_name)):
+            shapefile = open(os.path.join(PUMAS_ZIP_FILE_DIRECTORY, file_name), 'rb')
             # use ogre to convert zipped shapefile to geojson
             try:
-                converter_res = requests.post(converter, files={'upload': (file_name, shapefile)}, timeout=10)
+                converter_res = requests.post(SHAPEFILE_TO_GEOJSON_CONVERTER, 
+                                              files={'upload': (file_name, shapefile)}, timeout=100)
+            # except request timeout
             except requests.ReadTimeout:
                 print(" ... conversion failed, request timed out")
                 continue
-
+            # good conversion
             if converter_res.status_code == 200:
                 with open(json_file_path, 'wb') as geojson:
                     geojson.write(converter_res.content)
@@ -52,7 +55,7 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print('\nInterrupted, deleting incomplete conversions...\nExiting...')
         try:
-            os.remove(json_file_path)
+            os.remove(json_file_path)  # remove incomplete file if interrupted
         except FileNotFoundError:
             pass
         try:
--- download_pumas.py
+++ download_pumas.py
@@ -1,73 +1,41 @@
 import urllib.request
 import os
 import sys
-import time
+from utils.download_utils import ReportHook, fix_bad_zip_file
+from utils.globals import *
 
-with open("./puma_file_names.txt") as file_names:
+# script globals
+current_file = ""  # keep track of the current file being worked on
+# read in filenames to download from text file
+with open(PUMA_FILE_NAMES) as file_names:
     files = file_names.read().splitlines()
-
-base_url = "https://www2.census.gov/geo/tiger/TIGER2019/PUMA/"
-data_path = "./data/pumas/"
-zip_path = "./data/pumas/zip/"
-current_file = ""
-start_time = None
-
-
-def reporthook(count, block_size, total_size):
-    global start_time
-    global current_file
-    if count == 0:
-        start_time = time.time()
-        return
-    duration = time.time() - start_time
-    progress_size = int(count * block_size)
-    speed = int(progress_size / ((1024 * duration) + 1))
-    sys.stdout.write("\rDownloading file '%s' ... %d MB, %d KB/s, %0.2f sec elapsed" %
-                    (current_file, progress_size / (1024 * 1024), speed, duration))
-    sys.stdout.flush()
-
-
-def fix_bad_zip_file(zip_file):
-    f = open(zip_file, 'r+b')
-    data = f.read()  
-    pos = data.find(b'\x50\x4b\x05\x06') # End of central directory signature  
-    if pos > 0:
-        print(" ... Truncating file at location " + str(pos + 22)+ ".", end="")  
-        f.seek(pos + 22)   # size of 'ZIP end of central directory record' 
-        f.truncate()  
-        f.close()  
-    else:  
-        # raise error, file is truncated  
-        print(" ... Error: File is truncated")
 
 
 def main():
-    # create data path folder if it doesn't already exist
-    try:
-        os.mkdir(data_path)
-    except FileExistsError:
-        pass
-    try:
-        os.mkdir(zip_path)
-    except FileExistsError:
-        pass
+    # create data path folders if they don't already exist
+    for directory in [DATA_DIRECTORY, PUMAS_GEOJSON_DIRECTORY, PUMAS_ZIP_FILE_DIRECTORY]:
+        try:
+            os.mkdir(directory)
+        except FileExistsError:
+            pass
 
     for file_name in files:
         global current_file
         current_file = file_name
-        file_path = ""
         
         # download file if it doesn't already exist
-        if os.path.exists(os.path.join(data_path, file_name)):
-            file_path = os.path.join(data_path, file_name)
+        if os.path.exists(os.path.join(PUMAS_GEOJSON_DIRECTORY, file_name)):
+            file_path = os.path.join(PUMAS_GEOJSON_DIRECTORY, file_name)
             print("Downloading file '%s' ... done!" % file_name)
-        elif os.path.exists(os.path.join(zip_path, file_name)):
-            file_path = os.path.join(zip_path, file_name)
+        elif os.path.exists(os.path.join(PUMAS_ZIP_FILE_DIRECTORY, file_name)):
+            file_path = os.path.join(PUMAS_ZIP_FILE_DIRECTORY, file_name)
             print("Downloading file '%s' ... done!" % file_name)
         else:
             try:
-                file_path = os.path.join(data_path, file_name)
-                urllib.request.urlretrieve((base_url + file_name), file_path, reporthook=reporthook)
+                file_path = os.path.join(PUMAS_GEOJSON_DIRECTORY, file_name)
+                report_hook = ReportHook()
+                report_hook.current_file = current_file
+                urllib.request.urlretrieve((PUMAS_DOWNLOAD_URL + file_name), file_path, reporthook=report_hook.reporthook)
                 print(" ... done!")
                 sys.stdout.flush()
             except urllib.error.HTTPError:
@@ -75,9 +43,24 @@ def main():
                 continue
         
         # move zip file to ./data/puma/zip if it's in ./data/puma
-        new_file_path = os.path.join(zip_path, file_name)
+        new_file_path = os.path.join(PUMAS_ZIP_FILE_DIRECTORY, file_name)
         if file_path != new_file_path:
             os.rename(file_path, new_file_path)
+
+    # prompt user to delete leftover zip files to free up space
+    del_zips = ""
+    while del_zips != "y" and del_zips != "n":
+        del_zips = input("Do you wish to delete leftover zip files? (y/n): ")
+
+    files_in_directory = os.listdir(PUMAS_ZIP_FILE_DIRECTORY)
+    zip_files = [f for f in files_in_directory if f.endswith(".zip")]
+    if del_zips == "y":
+        print("Removing leftover zip files.")
+        for f in zip_files:
+            path_to_file = os.path.join(PUMAS_ZIP_FILE_DIRECTORY, f)
+            os.remove(path_to_file)
+    else:
+        print("Leftover zip files transfered to '%s'." % PUMAS_ZIP_FILE_DIRECTORY)
 
 
 if __name__ == '__main__':
@@ -86,7 +69,7 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print('\nInterrupted, deleting incomplete downloads...\nExiting...')
         try:
-            os.remove(data_path + current_file)
+            os.remove(PUMAS_GEOJSON_DIRECTORY + current_file)
         except FileNotFoundError:
             pass
         try:
