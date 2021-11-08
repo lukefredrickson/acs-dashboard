a7446cec101234d496b223bc9de0ffaa2db73a4d
Luke Fredrickson <lukeefredrickson@gmail.com>
2021-11-08 11:15:58 -0500

    readability changes to download_pumas.py

--- download_pumas.py
+++ download_pumas.py
@@ -4,12 +4,10 @@ import sys
 import zipfile
 from utils.download_utils import ReportHook
 from utils.globals import *
+from urllib import error, request
 
 # script globals
 current_file = ""  # keep track of the current file being worked on
-# read in filenames to download from text file
-with open(PUMAS_FILE_NAMES) as file_names:
-    files = file_names.read().splitlines()
 
 
 def main():
@@ -20,29 +18,34 @@ def main():
         except FileExistsError:
             pass
 
+    # read in filenames to download from text file
+    with open(PUMAS_FILE_NAMES) as file_names:
+        files = file_names.read().splitlines()
+
+    # download and extract shapefile from zips
     for file_name in files:
         global current_file
         current_file = file_name
-        # download file if it doesn't already exist
+        # download zip file if it doesn't already exist
         if os.path.exists(os.path.join(PUMAS_ZIP_FILE_DIRECTORY, file_name)):
             file_path = os.path.join(PUMAS_ZIP_FILE_DIRECTORY, file_name)
             print("Downloading file '%s' ... done!" % file_name)
         else:
+            # attempt download
             try:
                 file_path = os.path.join(PUMAS_ZIP_FILE_DIRECTORY, file_name)
                 report_hook = ReportHook()
                 report_hook.current_file = current_file
-                urllib.request.urlretrieve((PUMAS_DOWNLOAD_URL + file_name), file_path, reporthook=report_hook.reporthook)
+                request.urlretrieve((PUMAS_DOWNLOAD_URL + file_name), file_path, reporthook=report_hook.reporthook)
                 print(" ... done!")
                 sys.stdout.flush()
             except urllib.error.HTTPError:
                 print("Downloading file '%s' ... failed, invalid file!" % file_name)
                 continue
 
-        # unzip file
+        # extract shapefile from zip and move it to the shp/ directory
         with zipfile.ZipFile(file_path, 'r') as zip_ref:
-            base_file_name, _ = os.path.splitext(file_name)
-            shp_file_name = base_file_name + ".shp"
+            shp_file_name = os.path.splitext(file_name)[0] + ".shp"
             zip_ref.extract(shp_file_name, PUMAS_SHAPE_FILE_DIRECTORY)
         print(" ... zip file extracted!")
 
